307884500eecd6f9853e20dbea4a118cb44dcf9b
Francisco Gomes Pereira <61231527+fgomespereira@users.noreply.github.com>
2022-10-11 10:03:46 +0100

    PEPP regs included

--- P1 - Regs Bank-Level.R
+++ P1 - Regs Bank-Level.R
@@ -16,7 +16,7 @@ tmp2$app_dummy <- ifelse(tmp2$Year==2015 |
                            tmp2$Year==2017 |
                            tmp2$Year==2018, 1, 0)
 
-# Remove from 2012 to 2018
+# Keep 2012 to 2018 (APP period)
 tmp2 <- subset(tmp2, Year>=2012 & Year<=2018)
 
 # Make Year as factor
@@ -147,6 +147,137 @@ etable(reg_1, reg_2, reg_3, reg_4, reg_5, reg_6, reg_7, reg_8, reg_9, reg_10, re
        drop = c("Country"), # suppress Country*Year coefficients 
        postprocess.df = pandoc.table.return, style = "rmarkdown")
 
+###################################
+#### PEPP Analysis Regressions ####
+###################################
+
+#Import csv file from github repository
+myfile <- getURL("https://raw.githubusercontent.com/fgomespereira/P1---Unconventional-Monetary-Policy-in-the-EA/main/Panel%201.csv")
+panel1 <- read.csv(text = myfile)
+tmp2 <- panel1
+
+# PEPP_DUMMY setting
+tmp2$pepp_dummy <- ifelse(tmp2$Year==2020, 1, 0)
+
+# Keep 2019 and 2020 (PEPP period)
+tmp2 <- subset(tmp2, Year>=2019)
+
+reg_1 <- feols(Loans_G ~
+                        log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        Exposure_pepp * pepp_dummy |
+                        Bank + Year,
+                        data = tmp2)
+
+reg_2 <- feols(Loans_G ~
+                        log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        treat_50_pepp * pepp_dummy |
+                        Bank + Year,
+                        data = tmp2)
+
+reg_3 <- feols(Loans_G ~
+                       log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        treat_75_pepp * pepp_dummy |
+                        Bank + Year,
+                        data = tmp2)
+
+reg_4 <- feols(Loans_G ~
+                        log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        Exposure_pepp * pepp_dummy +
+                        Country * Year |
+                        Bank + Year,
+                        data = tmp2)
+
+reg_5 <- feols(Loans_G ~
+                        log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        treat_50_pepp * pepp_dummy +
+                        Country * Year |
+                        Bank + Year,
+                        data = tmp2)
 
 
+reg_6 <- feols(Loans_G ~
+                        log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        treat_75_pepp * pepp_dummy +
+                        Country * Year |
+                        Bank + Year,
+                        data = tmp2)
 
+reg_7 <- feols(L_over_A_G ~
+                        log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        Exposure_pepp * pepp_dummy |
+                        Bank + Year,
+                        data = tmp2)
+
+reg_8 <- feols(L_over_A_G ~
+                        log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        treat_50_pepp * pepp_dummy |
+                        Bank + Year,
+                        data = tmp2)
+
+reg_9 <- feols(L_over_A_G ~
+                       log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        treat_75_pepp * pepp_dummy |
+                        Bank + Year,
+                        data = tmp2)
+
+reg_10 <- feols(L_over_A_G ~
+                        log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        Exposure_pepp * pepp_dummy +
+                        Country * Year |
+                        Bank + Year,
+                        data = tmp2)
+
+reg_11 <- feols(L_over_A_G ~
+                        log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        treat_50_pepp * pepp_dummy +
+                        Country * Year |
+                        Bank + Year,
+                        data = tmp2)
+
+
+reg_12 <- feols(L_over_A_G ~
+                        log(Assets)  +
+                        Equity_over_A +
+                        ROA +
+                        Deposits_over_A +
+                        treat_75_pepp * pepp_dummy +
+                        Country * Year |
+                        Bank + Year,
+                        data = tmp2)
+# Regression tables
+etable(reg_1, reg_2, reg_3, reg_4, reg_5, reg_6, reg_7, reg_8, reg_9, reg_10, reg_11, reg_12,
+       drop = c("Country"), # suppress Country*Year coefficients 
+       postprocess.df = pandoc.table.return, style = "rmarkdown")
